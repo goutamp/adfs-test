c02c42413fd7f846b6f1c643fb648e1343ec2e1b
goutamp <pramanick.goutam@gmail.com>
2020-01-28 10:41:35 +0530

    update the template file path

--- adfs-custom/iam.tf
+++ adfs-custom/iam.tf
@@ -28,7 +28,7 @@ resource "aws_iam_role" "support_role" {
 }
 
 data "template_file" "saml_policy" {
-  template = "${file("./adfs-test/templates/assume-saml.json")}"
+  template = "${file(".terraform/assume-saml.json")}"
 
   vars = {
     account_id    = "${data.aws_caller_identity.this.account_id}"
@@ -47,7 +47,7 @@ resource "aws_iam_role_policy" "support_policy" {
 }
 
 data "template_file" "support_policy" {
-  template = "${file("./adfs-test/templates/support-policy.json")}"
+  template = "${file(".terraform/support-policy.json")}"
 
   vars = {
     account_id = "${data.aws_caller_identity.this.account_id}"
